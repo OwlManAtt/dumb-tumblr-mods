7cd1214b8db1c3dfac042614804a6e0708d74e60
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2014-10-28 02:39:47 +0000

    Userscript to post to a secondary blog by default.

---
+++ README.md
@@ -0,0 +1,8 @@
+Dumb Tumblr Mods
+----------------
+
+This will probably grow into a whole collection of things at some point, assuming I keep tumbling. These scripts are tested using Greasemonkey under Firefox. They probably work on Chrome. Feel free to send a PR with fixes if they break on strange browsers.
+
+Right now, we have:
+
+* ``default_blog.user.js`` - Changes the blog you post to by default. My primary blog is for an organization website (yes, I suck at tumblr) and I constantly reblog posts for my personal (secondary) blog onto it like an idiot.
---
+++ default_blog.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name        Tumblr - Change default blog
+// @namespace   yshi
+// @include     https://www.tumblr.com/*
+// @version     1
+// @description Changes the default blog to one of your secondary blogs when posting/reblogging stuff. 
+// @grant       none
+// ==/UserScript==
+
+// Found on <https://github.com/OwlManAtt/dumb-tumblr-mods>.
+
+// Change this to the data-option-value for the blog you'd like to default to.
+var defaultBlogDataOptionValue = 'yshijustice';
+
+/* ========== !! Don't play with it after this point if you don't know how to computer !! ========== */
+var $ = unsafeWindow.jQuery;
+
+var changeDefault = (function(){
+    secondaryBlog = $('div[data-option-value=' + defaultBlogDataOptionValue + ']');
+    
+    if(secondaryBlog.length > 0){
+      secondaryBlog.click();
+    }
+});
+
+// Lifted from <http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom/14570614#14570614>
+var observeDOM = (function(){
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+        eventListenerSupported = window.addEventListener;
+    
+    return function(obj, callback){
+        if( MutationObserver ){
+            // define a new observer
+            var obs = new MutationObserver(function(mutations, observer){
+                if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+                    // Disable for the duration of the callback so we can mutate the page without self-notifying
+                    obs.disconnect();
+                    callback();
+                    obs.observe( obj, { childList:true, subtree:true });
+            });
+            // have the observer observe foo for changes in children
+            obs.observe( obj, { childList:true, subtree:true });
+        }
+        else if( eventListenerSupported ){
+            obj.addEventListener('DOMNodeInserted', callback, false);
+            // obj.addEventListener('DOMNodeRemoved', callback, false);
+        }
+    }
+})();
+
+// Observe a specific DOM element:
+observeDOM(document.body, changeDefault);
